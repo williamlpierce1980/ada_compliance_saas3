2cbc75328982edbab167e71814b2b9fa24d0dd6a
williamlpierce1980 <williamlpierce1980@gmail.com>
2025-07-24 19:37:40 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from flask import Flask, send_from_directory
 from flask_cors import CORS
-from src.models.user import db
 from src.routes.user import user_bp
 from src.routes.compliance import compliance_bp
 
