eb7c3b8c2c077273445dc121def9fec431be8768
williamlpierce1980 <williamlpierce1980@gmail.com>
2025-07-24 20:12:01 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from flask import Flask, send_from_directory
 from flask_cors import CORS
-from src.routes.user import user_bp
+#from src.routes.user import user_bp
 from src.routes.compliance import compliance_bp
 
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
